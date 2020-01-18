a2bb77b124f005879c3ddadaa20568f01520fc12
David Koeplinger <dkoeplinger@gmail.com>
2020-01-18 00:45:23 -0800

    Update format

--- cmake/AddFormatTargets.cmake
+++ cmake/AddFormatTargets.cmake
@@ -4,41 +4,43 @@ function (add_format_targets SOURCES EXCLUDESOURCES TARGET_PREFIX)
         find_program(CLANG_FORMAT "clang-format")
         if(CLANG_FORMAT)
             add_custom_target(
-                    # show the list of files will be formatted (without inplace format)
-                    ${TARGET_PREFIX}-clang-format
-                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r -q --diff -v
-                    --clang-format-options="-style=file"
-                    # add the following line to exclude formatting sepcific files
-                    --exclude="${EXCLUDESOURCES}"
-                    ${SOURCES}
-            )
-            add_custom_target(
                     # inplace format the files
-                    ${TARGET_PREFIX}-clang-format-inplace
+                    ${TARGET_PREFIX}-format
                     ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r -q --inplace -v
                     --clang-format-options="-style=file"
-                    # add the following line to exclude formatting sepcific files
+                    # add the following line to exclude formatting specific files
                     --exclude="${EXCLUDESOURCES}"
                     ${SOURCES}
             )
-            add_custom_target(
-                    # show the fomratted diff of the files (without inplace format)
-                    ${TARGET_PREFIX}-clang-format-diff
-                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r --diff
-                    --clang-format-options="-style=file"
-                    # add the following line to exclude formatting sepcific files
-                    --exclude="${EXCLUDESOURCES}"
-                    ${SOURCES}
-            )
-            add_custom_target(
-                    # check if there are files not formatted (fail if there are)
-                    ${TARGET_PREFIX}-clang-format-checkdiff
-                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r -q -v --faildiff
-                    --clang-format-options="-style=file"
-                    # add the following line to exclude formatting sepcific files
-                    --exclude="${EXCLUDESOURCES}"
-                    ${SOURCES}
-            )
+
+#            add_custom_target(
+#                    # show the list of files will be formatted (without inplace format)
+#                    ${TARGET_PREFIX}-format-list
+#                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r -q --diff -v
+#                    --clang-format-options="-style=file"
+#                    # add the following line to exclude formatting sepcific files
+#                    --exclude="${EXCLUDESOURCES}"
+#                    ${SOURCES}
+#            )
+#
+#            add_custom_target(
+#                    # show the fomratted diff of the files (without inplace format)
+#                    ${TARGET_PREFIX}-format-diff
+#                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r --diff
+#                    --clang-format-options="-style=file"
+#                    # add the following line to exclude formatting sepcific files
+#                    --exclude="${EXCLUDESOURCES}"
+#                    ${SOURCES}
+#            )
+#            add_custom_target(
+#                    # check if there are files not formatted (fail if there are)
+#                    ${TARGET_PREFIX}-format-check
+#                    ${CMAKE_SOURCE_DIR}/cmake/run-clang-format.py -r -q -v --faildiff
+#                    --clang-format-options="-style=file"
+#                    # add the following line to exclude formatting sepcific files
+#                    --exclude="${EXCLUDESOURCES}"
+#                    ${SOURCES}
+#            )
         else(CLANG_FORMAT)
             message(FATAL_ERROR "Cannot find clang-format")
         endif(CLANG_FORMAT)
